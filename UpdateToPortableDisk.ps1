45e128ef66929b2f426a84d299d23e16e610f937
sdrkjsph <71620593+mrpointzby@users.noreply.github.com>
2023-10-16 13:59:23 +0800

    Add files via upload

--- UpdateToPortableDisk.ps1
+++ UpdateToPortableDisk.ps1
@@ -1,3 +1,4 @@
+<#V1:
 $ArrayDisk = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -ExpandProperty DeviceID
 $i = 0
 while($i -lt $ArrayDisk.Length){
@@ -5,3 +6,16 @@ copy-item -Recurse C:\Users\Brady\Desktop\Xter_tools_prototype\ "$($ArrayDisk[$i
 copy-item -Recurse C:\Users\Brady\Desktop\CurrentIssue\ "$($ArrayDisk[$i])\"
 $i ++
 }
+#>
+
+$ArrayDisk = Get-PSDrive -PSProvider FileSystem | where {$_.Free -gt 200GB} | select -ExpandProperty Root
+$i = 0
+while($i -lt $ArrayDisk.Length){
+    copy-item -Recurse C:\Users\Brady\Desktop\Xter_tools_prototype\ "$($ArrayDisk[$i])"
+    copy-item -Recurse C:\Users\Brady\Desktop\CurrentIssue\ "$($ArrayDisk[$i])"
+}
+
+# OneDrive Transport
+copy-item -Recurse C:\Users\Brady\Desktop\CurrentIssue\ "C:\Users\Brady\OneDrive - Foxconn\文件"
+copy-item -Recurse C:\Users\Brady\Desktop\Xter_tools_prototype\ "C:\Users\Brady\OneDrive - Foxconn\文件"
+copy-item -Recurse C:\Users\Brady\Desktop\WorkAndHome\ "C:\Users\Brady\OneDrive - Foxconn\文件"
